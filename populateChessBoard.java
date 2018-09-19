e6a7a389fab3ba6f328d62dae31f2ec23eb30eaa
kevin115533 <kevin115533@yahoo.com>
2018-09-19 16:08:30 -0500

    positions still need work

--- populateChessBoard.java
+++ populateChessBoard.java
@@ -1,24 +1,39 @@
 public class populateChessBoard {
 	public static void main (String[] args){
 		
-		int bCount = 15;
+		int count = 15;
 		char nonPawn[] = {'R','N','B','Q','K','B','N','R'};
+		String position[] ={"a1","b1","c1","d1","e1","f1","g1","h1"};
 	
 		
-		ChessPiece[] black = new ChessPiece[bCount];
+		ChessPiece[] black = new ChessPiece[count];
 		
-		for(int i = 0;i<bCount; i++){
+		for(int i = 0;i<count; i++){
 			black[i] = new ChessPiece();
 			if(i<=7){
-				black[i].makePiece("b",nonPawn[i],"a1");
+				black[i].makePiece("b",nonPawn[i],position[i]);
 			}
 			else{			
 				black[i].makePiece("b",'P',"a1");
 			}
 		}
 		
-		for(int i = 0;i<bCount; i++){
+		ChessPiece[] white = new ChessPiece[count];
+		
+		for(int i = 0;i<count; i++){
+			white[i] = new ChessPiece();
+			if(i<=7){
+				white[i].makePiece("w",nonPawn[i],"a1");
+			}
+			else{			
+				white[i].makePiece("w",'P',"a1");
+			}
+		}
+		
+		for(int i = 0;i<count; i++){
 			System.out.println(black[i].printInfo());
+			//System.out.print(" ");
+			
 		}
 		
 	}
