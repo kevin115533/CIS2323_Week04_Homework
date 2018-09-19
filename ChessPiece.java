64a98b66c2002677a218954ed847c76b918a5797
kevin115533 <kevin115533@yahoo.com>
2018-09-18 21:51:49 -0500

    first

---
+++ ChessPiece.java
@@ -0,0 +1,41 @@
+public class ChessPiece{
+	
+	private String pos;
+	private String color, type;
+	
+	public void makePiece (String x, String y, String z){
+		setColor(x);
+		setPosition(z);
+		setPieceType(y);
+	}
+	
+	public void setPosition(String x){
+		pos = x;
+	}
+	
+	public String getPosition(){
+		return pos;
+	}
+	
+	public void setColor(String x){
+		color = x;
+	}
+	
+	public String getColor(){
+		return color;
+	}
+	
+	public void setPieceType(String x){
+		type = x;
+	}
+	
+	public String getPieceType(){
+		return type;
+	}
+	
+	public String printInfo(){
+		return "Color: " + color + "\n"
+		+ "Piece : " + type + "\n"
+		+ "Position: " + pos; 
+	}
+}	
